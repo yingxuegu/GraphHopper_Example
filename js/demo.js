78a4bd6a68d4396e79c86270b5bd4f52b6380c71
Xueying Gu <xueying.gu@maine.edu>
2015-05-06 13:09:04 -0400

    update

--- js/demo.js
+++ js/demo.js
@@ -1,94 +1,121 @@
+
+
 $(document).ready(function (e) {
-    jQuery.support.cors = true;
+    var routingMap = createMap('routing-map');
+    lmap = routingMap;
+    setupRoutingAPI(lmap, ghRouting);
+});
 
-    $(".tab-content").css("display", "none");
-    $(".tabs-menu a").click(function (event) {
-        // event.preventDefault();
-        $(this).parent().addClass("current");
-        $(this).parent().siblings().removeClass("current");
-        var tab = $(this).attr("href");
-        $(".tab-content").not(tab).css("display", "none");
-        $(tab).fadeIn();
+function locate(map){
+    var routingLayer = L.geoJson().addTo(map);
+    routingLayer.options = {
+        style: {color: "#00cc33", "weight": 5, "opacity": 0.6}
+    };
+    var ev = {lat:orilat, lng:orilng};
 
-        // a bit hackish to refresh the map
-        routingMap.invalidateSize(false);
-        vrpMap.invalidateSize(false);
+    var iconObject = L.icon({
+        iconUrl: './img/marker-icon-green.png',
+        shadowSize: [50, 64],
+        shadowAnchor: [4, 62],
+        iconAnchor: [12, 8]
     });
 
-    var host = "https://graphhopper.com/api/1";
+    L.marker(ev, {icon: iconObject}).addTo(routingLayer);
+    map.on('load',  L.marker(ev, {icon: iconObject}).addTo(routingLayer)
 
-    if (!host) {
-        if (location.port === '') {
-            host = location.protocol + '//' + location.hostname;
-        } else {
-            host = location.protocol + '//' + location.hostname + ":" + location.port;
-        }
+    );
+
+    if (ghRouting.points.length > 1) {
+        ghRouting.clearPoints();
+        routingLayer.clearLayers();
     }
 
-    //
-    // Sign-up for free and get your own key: https://graphhopper.com/#directions-api
-    //
-    var defaultKey = "2929232f-74f8-44cc-8850-76709888e2de";
-    var profile = "bike";
+    ghRouting.addPoint(new GHInput(orilat, orilng));
+    if (ghRouting.points.length > 1) {
+        // ******************
+        //  Calculate route!
+        // ******************
+        ghRouting.doRequest(function (json) {
+            if (json.message) {
+                var str = "An error occured: " + json.message;
+                if (json.hints)
+                    str += json.hints;
 
-    // create a routing client to fetch real routes, elevation.true is only supported for vehicle bike or foot
-    var ghRouting = new GraphHopperRouting({key: defaultKey, host: host, vehicle: profile, elevation: false});
+                $("#routing-response").text(str);
 
-    var ghOptimization = new GraphHopperOptimization({key: defaultKey, host: host, profile: profile});
+            } else {
+                var path = json.paths[0];
+                var str = JSON.stringify(json, null, 2);;
+                //alert(che);
+                routingLayer.addData({
+                    "type": "Feature",
+                    "geometry": path.points
+                });
+                var outHtml = "Distance in meter:" + path.distance;
+                outHtml += "<br/>Times in seconds:" + path.time / 1000;
+                $("#routing-response").html(outHtml);
 
-    var ghGeocoding = new GraphHopperGeocoding({key: defaultKey, host: host, limit: 8, locale: "en" /* currently fr, en, de and it are explicitely supported */});
+                if (path.bbox) {
+                    var minLon = path.bbox[0];
+                    var minLat = path.bbox[1];
+                    var maxLon = path.bbox[2];
+                    var maxLat = path.bbox[3];
+                    var tmpB = new L.LatLngBounds(new L.LatLng(minLat, minLon), new L.LatLng(maxLat, maxLon));
+                    map.fitBounds(tmpB);
+                }
 
-    var ghMatrix = new GraphHopperMatrix({key: defaultKey, host: host, vehicle: profile});
+                instructionsDiv.empty();
+                if (path.instructions) {
+                    var allPoints = path.points.coordinates;
+                    var listUL = $("<ol>");
+                    instructionsDiv.append(listUL);
+                    for (var idx in path.instructions) {
+                        var instr = path.instructions[idx];
 
-    var overwriteExistingKey = function () {
-        var key = $("#custom_key_input").val();
-        if (key && key !== defaultKey) {
-            $("#custom_key_enabled").show();
+                        // use 'interval' to find the geometry (list of points) until the next instruction
+                        var instruction_points = allPoints.slice(instr.interval[0], instr.interval[1]);
 
-            ghRouting.key = key;
-            ghMatrix.key = key;
-            ghGeocoding.key = key;
+                        // use 'sign' to display e.g. equally named images
 
-            // TODO make identical
-            ghOptimization.setKey(key);
-        } else {
-            $("#custom_key_enabled").hide();
-        }
-    };
-    overwriteExistingKey();
-    $("#custom_key_button").click(overwriteExistingKey);
-
-    var routingMap = createMap('routing-map');
-    setupRoutingAPI(routingMap, ghRouting);
-
-    var vrpMap = createMap('vrp-map');
-    setupTourOptimizationAPI(vrpMap, ghOptimization, ghRouting);
-
-    setupGeocodingAPI(ghGeocoding);
-
-    setupMatrixAPI(ghMatrix);
-});
+                        $("<li>" + instr.text + " <small>(" + ghRouting.getTurnText(instr.sign) + ")</small>"
+                            + " for " + instr.distance + "m and " + Math.round(instr.time / 1000) + "sec"
+                            + ", geometry points:" + instruction_points.length + "</li>").
+                            appendTo(listUL);
+                    }
+                }
+            }
+        });
+    }
+}
 
 function setupRoutingAPI(map, ghRouting) {
     map.setView([44.897912, -68.670838], 16);
-    var iconObject = L.icon({
-        iconUrl: './img/marker-icon.png',
-        shadowSize: [50, 64],
-        shadowAnchor: [4, 62],
-        iconAnchor: [12, 40]
-    });
+    var ev = {lat: 44.8969363, lng: -68.66730580000001};
+
+    var routingLayer = L.geoJson().addTo(map);
+    routingLayer.options = {
+        style: {color: "#00cc33", "weight": 5, "opacity": 0.6}
+    };
+    //map.on('load',  L.marker(ev, {icon: iconObject}).addTo(routingLayer)
+   // );
     var instructionsDiv = $("#instructions");
+    //alert(instructionsDiv);
     map.on('click', function (e) {
+       // alert("a");
         if (ghRouting.points.length > 1) {
             ghRouting.clearPoints();
             routingLayer.clearLayers();
         }
+        //alert(JSON.stringify(e.latlng, null, 4));
+        //alert(e.latlng.lat);
+       // var ev = {lat: 44.8969363, lng: -68.66730580000001};
+       // L.marker(ev, {icon: iconObject}).addTo(routingLayer);
 
         L.marker(e.latlng, {icon: iconObject}).addTo(routingLayer);
         ghRouting.addPoint(new GHInput(e.latlng.lat, e.latlng.lng));
         if (ghRouting.points.length > 1) {
             // ******************
-            //  Calculate route! 
+            //  Calculate route!
             // ******************
             ghRouting.doRequest(function (json) {
                 if (json.message) {
@@ -100,13 +127,14 @@ function setupRoutingAPI(map, ghRouting) {
 
                 } else {
                     var path = json.paths[0];
+                    var str = JSON.stringify(json, null, 2);
+                    //alert(che);
                     routingLayer.addData({
                         "type": "Feature",
                         "geometry": path.points
                     });
                     var outHtml = "Distance in meter:" + path.distance;
                     outHtml += "<br/>Times in seconds:" + path.time / 1000;
-                    outHtml += "<br/><a href='" + ghRouting.getGraphHopperMapsLink() + "'>GraphHopper Maps</a>";
                     $("#routing-response").html(outHtml);
 
                     if (path.bbox) {
@@ -130,7 +158,7 @@ function setupRoutingAPI(map, ghRouting) {
                             var instruction_points = allPoints.slice(instr.interval[0], instr.interval[1]);
 
                             // use 'sign' to display e.g. equally named images
-
+                            alert(instr.sign);
                             $("<li>" + instr.text + " <small>(" + ghRouting.getTurnText(instr.sign) + ")</small>"
                                     + " for " + instr.distance + "m and " + Math.round(instr.time / 1000) + "sec"
                                     + ", geometry points:" + instruction_points.length + "</li>").
@@ -147,293 +175,9 @@ function setupRoutingAPI(map, ghRouting) {
         instructionsDiv.toggle();
     });
 
-    var routingLayer = L.geoJson().addTo(map);
-    routingLayer.options = {
-        style: {color: "#00cc33", "weight": 5, "opacity": 0.6}
-    };
+
 }
 
-function setupTourOptimizationAPI(map, ghOptimization, ghRouting) {
-    map.setView([51.505, -0.09], 13);
-
-    L.NumberedDivIcon = L.Icon.extend({
-        options: {
-            iconUrl: './img/marker-icon.png',
-            number: '',
-            shadowUrl: null,
-            iconSize: new L.Point(25, 41),
-            iconAnchor: new L.Point(13, 41),
-            popupAnchor: new L.Point(0, -33),
-            className: 'leaflet-div-icon'
-        },
-        createIcon: function () {
-            var div = document.createElement('div');
-            var img = this._createImg(this.options['iconUrl']);
-            var numdiv = document.createElement('div');
-            numdiv.setAttribute("class", "number");
-            numdiv.innerHTML = this.options['number'] || '';
-            div.appendChild(img);
-            div.appendChild(numdiv);
-            this._setIconStyles(div, 'icon');
-            return div;
-        },
-        // you could change this to add a shadow like in the normal marker if you really wanted
-        createShadow: function () {
-            return null;
-        }
-    });
-
-    var addPointToMap = function (lat, lng, index) {
-        index = parseInt(index);
-        if (index === 0) {
-            new L.Marker([lat, lng],
-                    {icon: new L.NumberedDivIcon({iconUrl: './img/marker-icon-green.png', number: '1'})}).
-                    addTo(routingLayer);
-        } else {
-            new L.Marker([lat, lng],
-                    {icon: new L.NumberedDivIcon({number: '' + (index + 1)})}).
-                    addTo(routingLayer);
-        }
-    };
-
-    map.on('click', function (e) {
-        addPointToMap(e.latlng.lat, e.latlng.lng, ghOptimization.points.length);
-        ghOptimization.addPoint(new GHInput(e.latlng.lat, e.latlng.lng));
-    });
-
-    var routingLayer = L.geoJson().addTo(map);
-    routingLayer.options.style = function (feature) {
-        return feature.properties && feature.properties.style;
-    };
-
-    var clearMap = function () {
-        ghOptimization.clear();
-        routingLayer.clearLayers();
-        ghRouting.clearPoints();
-        $("#vrp-response").text(" ");
-        $("#vrp-error").text(" ");
-    };
-
-    var optimizeResponse = function (json) {
-        if (json.message) {
-            $("#vrp-error").text("An error occured: " + json.message);
-            $("#vrp-response").text(" ");
-            console.log(JSON.stringify(json));
-            return;
-        }
-        var sol = json.solution;
-        if (!sol)
-            return;
-
-        $("#vrp-response").text("Solution found for " + sol.routes.length + " vehicle(s)! "
-                + "Distance: " + Math.floor(sol.distance / 1000) + "km, costs: " + sol.costs);
-
-        var no_unassigned = sol.unassigned.services.length + sol.unassigned.shipments.length;
-        if (no_unassigned > 0)
-            $("#vrp-error").append("<br/>unassigned jobs: " + no_unassigned);
-
-        routingLayer.clearLayers();
-        for (var routeIndex = 0; routeIndex < sol.routes.length; routeIndex++) {
-            var route = sol.routes[routeIndex];
-
-            // fetch real routes from graphhopper
-            ghRouting.clearPoints();
-            var firstAdd;
-            for (var actIndex = 0; actIndex < route.activities.length; actIndex++) {
-                var add = route.activities[actIndex].address;
-                ghRouting.addPoint(new GHInput(add.lat, add.lon));
-
-                if (!eqAddress(firstAdd, add))
-                    addPointToMap(add.lat, add.lon, actIndex);
-
-                if (actIndex === 0)
-                    firstAdd = add;
-            }
-
-            var routeStyle;
-            if (routeIndex === 2) {
-                routeStyle = {color: "yellow"};
-            } else if (routeIndex === 1) {
-                routeStyle = {color: "green"};
-            } else {
-                routeStyle = {color: "blue"};
-            }
-
-            var ghCallback = createGHCallback(routeStyle);
-            ghRouting.doRequest(ghCallback, {instructions: true});
-        }
-    };
-
-    var eqAddress = function (add1, add2) {
-        return add1 && add2
-                && Math.floor(add1.lat * 1000000) === Math.floor(add2.lat * 1000000)
-                && Math.floor(add1.lon * 1000000) === Math.floor(add2.lon * 1000000)
-    };
-
-    var createGHCallback = function (routeStyle) {
-        return function (json) {
-            if (json.message) {
-                var str = "An error for the routing occurred: " + json.message;
-                if (json.hints)
-                    str += json.hints;
-                $("#vrp-error").text(str);
-                console.log(JSON.stringify(json));
-
-            } else {
-                for (var pathIndex = 0; pathIndex < json.paths.length; pathIndex++) {
-                    var path = json.paths[pathIndex];
-                    routingLayer.addData({
-                        "type": "Feature",
-                        "geometry": path.points,
-                        "properties": {
-                            style: routeStyle
-                        }
-                    });
-                }
-            }
-        };
-    };
-
-    var optimizeTour = function () {
-        if (ghOptimization.points.length < 3) {
-            $("#vrp-response").text("At least 3 points required but was: " + ghOptimization.points.length);
-            return;
-        }
-        $("#vrp-response").text("Calculating ...");
-        ghOptimization.doTSPRequest(optimizeResponse);
-    };
-
-    $("#vrp_clear_button").click(clearMap);
-
-    $("#set_example_vrp").click(function () {
-        $.getJSON("tour-optimization-examples/vrp_lonlat_new.json", function (jsonData) {
-
-            clearMap();
-            map.setView([50.352471, 10.623779], 6);
-            $("#vrp-response").text("Calculating ...");
-            ghOptimization.doRequest(jsonData, optimizeResponse);
-        });
-    });
-
-    $("#set_example_tsp").click(function () {
-        $.getJSON("tour-optimization-examples/tsp_lonlat_new.json", function (jsonData) {
-
-            clearMap();
-            map.setView([50.352471, 10.623779], 6);
-            $("#vrp-response").text("Calculating ...");
-            ghOptimization.doRequest(jsonData, optimizeResponse);
-        });
-    });
-
-    $("#set_example_us_tour").click(function () {
-        $.getJSON("tour-optimization-examples/american_road_trip.json", function (jsonData) {
-
-            clearMap();
-            map.setView([38.754083, -101.074219], 4);
-            $("#vrp-response").text("Calculating ...");
-            ghOptimization.doRequest(jsonData, optimizeResponse);
-        });
-    });
-
-    $("#set_example_tsp2").click(function () {
-        $.getJSON("tour-optimization-examples/tsp_lonlat_end.json", function (jsonData) {
-
-            clearMap();
-            map.setView([50.352471, 10.623779], 6);
-            $("#vrp-response").text("Calculating ...");
-            ghOptimization.doRequest(jsonData, optimizeResponse);
-        });
-    });
-
-    $("#optimize_button").click(optimizeTour);
-}
-
-function setupGeocodingAPI(ghGeocoding) {
-    //  Find address
-    var textField = $("#geocoding_text_field");
-    $("#geocoding_search_button").click(function () {
-        $("#geocoding-results").empty();
-        $("#geocoding-error").empty();
-
-        $("#geocoding-response").empty();
-        ghGeocoding.doRequest(function (json) {
-            if (json.info && json.info.errors) {
-                $("#geocoding-error").text("An error occured: " + json.info.errors[0].message);
-            } else {
-                var listUL = $("<ol>");
-                $("#geocoding-response").append(listUL);
-                for (var hitIdx in json.hits) {
-                    var hit = json.hits[hitIdx];
-
-                    $("<li>" + dataToText(hit) + "</li>").appendTo(listUL);
-                }
-            }
-        }, {query: textField.val()});
-    });
-
-
-    function dataToText(data) {
-        var text = "";
-        if (data.name)
-            text += data.name;
-
-        if (data.postcode)
-            text = insComma(text, data.postcode);
-
-        // make sure name won't be duplicated
-        if (data.city && text.indexOf(data.city) < 0)
-            text = insComma(text, data.city);
-
-        if (data.country && text.indexOf(data.country) < 0)
-            text = insComma(text, data.country);
-        return text;
-    }
-
-    function insComma(textA, textB) {
-        if (textA.length > 0)
-            return textA + ", " + textB;
-        return textB;
-    }
-}
-
-function setupMatrixAPI(ghMatrix) {
-    $('#matrix_search_button').click(function () {
-
-        // possible out_array options are: weights, distances, times, paths
-        ghMatrix.addOutArray("distances");
-        ghMatrix.addOutArray("times");
-
-        ghMatrix.clearPoints();
-        $('.point').each(function (idx, div) {
-            // parse the input strings and adds it as from_point and to_point
-            ghMatrix.addPoint(new GHInput($(div).val()));
-
-            // To create an NxM matrix you can simply use the other methods e.g.
-            // ghm.addFromPoint(new GHInput(someCoordinateString))
-            // or
-            // ghm.addToPoint(new GHInput(someCoordinateString))
-        });
-
-        $("#matrix-error").empty();
-        $("#matrix-response").empty();
-
-        ghMatrix.doRequest(function (json) {
-            if (json.message) {
-                var str = "An error occured: " + json.message;
-                if (json.hints)
-                    str += json.hints;
-
-                $("#matrix-error").text(str);
-            } else {
-                var outHtml = "Distances in meters: <br/>" + ghMatrix.toHtmlTable(json.distances);
-                outHtml += "<br/><br/>Times in seconds: <br/>" + ghMatrix.toHtmlTable(json.times);
-                $("#matrix-response").html(outHtml);
-            }
-        });
-
-        return false;
-    });
-}
 
 function createMap(divId) {
     var osmAttr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
@@ -447,7 +191,11 @@ function createMap(divId) {
     });
 
     var map = L.map(divId, {layers: [mapquest]});
-    L.control.layers({"MapQuest": mapquest,
-        "OpenMapSurfer": openMapSurfer, }).addTo(map);
     return map;
+}
+
+
+function testtest(){
+    //var routingMap = createMap'routing-map');
+    locate(lmap);
 }
